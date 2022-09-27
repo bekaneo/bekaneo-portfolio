95b753c39e694ed6b9949bc510fd962ac129fe27
bekaneo <vealtto@gmail.com>
2022-09-27 17:55:20 +0600

    Added base.html with boorstrap

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -55,18 +55,18 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["personal_portfolio/templates/"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
